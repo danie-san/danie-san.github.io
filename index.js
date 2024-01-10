a31fddc15fb3fb1b149b530323479265c7fb4b60
Danielle Santos <danielleliz1721@gmail.com>
2024-01-10 14:52:53 -0500

    finished about section

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
-function openTab(name) {
+function openTab(tabName) {
     for(tablink of tablinks){
         tablink.classList.remove("active-link")
     }
 
     for(tabcontent of tabcontents){
-        tabcontents.classList.remove("active-tab")
+        tabcontent.classList.remove("active-tab")
     }
 
-    Event.currentTarget.classList.add("active-link");
-    document.getElementById(name).classList.add("active-tab");
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab");
 }
