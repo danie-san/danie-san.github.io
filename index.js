9f391d0cd3bc7fa5120c0f19426b4c58cfeae421
Danielle Santos <danielleliz1721@gmail.com>
2024-01-10 00:01:47 -0500

    js function

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 
 var tablinks = document.getElementsByClassName("tab-links");
-var tablinks = document.getElementsByClassName("tab-content");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function openTab(name) {
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+
+    for(tabcontent of tabcontents){
+        tabcontents.classList.remove("active-tab")
+    }
+
+    Event.currentTarget.classList.add("active-link");
+    document.getElementById(name).classList.add("active-tab");
+}
